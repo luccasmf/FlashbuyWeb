e03644b6d95d00a16e9a9eca7872405ed89ff2bc
whenriquebs <w.henriquebs@gmail.com>
2016-03-19 01:15:41 -0300

    Criada a LayoutPageAnunciante, View /Anunciante/Index (será o painel de controle), Controller Anunciante já com o método Index para a respectiva View.

---
+++ FlashBuy/Controllers/AnuncianteController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FlashBuyClassLibrary;
+
+namespace FlashBuy.Controllers
+{
+    public class AnuncianteController : Controller
+    {
+        // GET: Anunciante
+        public ActionResult Index()
+        {
+            var listaOfertas = new HashSet<Oferta>();
+            return View(listaOfertas);
+        }
+    }
+}
