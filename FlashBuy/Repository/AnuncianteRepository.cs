ed13f97b66e3a2d5c2197dc3210a08542c61297d
Helena Maura Carvalho <helenamaura@gmail.com>
2016-03-27 16:37:41 -0300

    Cadastro de anunciante funcionando. Pendentes questões anotadas na controller como TODO.

---
+++ FlashBuy/Repository/AnuncianteRepository.cs
@@ -0,0 +1,32 @@
+﻿using FlashBuyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FlashBuy.Repository
+{
+    public class AnuncianteRepository
+    {
+        internal bool GetAnuncianteByEmail(string email)
+        {
+            FlashBuyModel context = new FlashBuyModel();
+            return context.Anunciante.Any(a => a.Email == email);
+        }
+
+        internal void Salvar(Anunciante model)
+        {
+            try
+            {
+                FlashBuyModel context = new FlashBuyModel();
+                context.Anunciante.Add(model);
+                context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
