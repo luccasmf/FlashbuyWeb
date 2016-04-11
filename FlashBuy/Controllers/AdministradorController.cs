d62a343793a370c47992008a1d6cb8cfee17b8f1
Felipe <felipebranko@hotmail.com>
2016-04-11 20:29:35 -0300

    Criação das estruturas de páginas de administrador

---
+++ FlashBuy/Controllers/AdministradorController.cs
@@ -0,0 +1,23 @@
+﻿using FlashBuy.Repository;
+using FlashBuyClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FlashBuy.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        AdministradorRepository repositorio = new AdministradorRepository();
+
+        // GET: Administrador
+        public ActionResult Index()
+        {
+            List<Oferta> ofertas = repositorio.GetOfertas();
+            var listaOfertas = new HashSet<Oferta>(ofertas);
+            return View(listaOfertas);
+        }
+    }
+}
---
+++ FlashBuy/Repository/AdministradorRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FlashBuyClassLibrary;
+
+namespace FlashBuy.Repository
+{
+    public class AdministradorRepository
+    {
+        internal List<Oferta> GetOfertas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
